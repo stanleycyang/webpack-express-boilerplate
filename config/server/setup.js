8d682d4af17346a2766c77dbae68b58298f411d7
stanleycyang <stanley@stanleycyang.com>
2015-09-20 17:09:03 -0700

    Added modular functions into the bin/dev

---
+++ config/server/setup.js
@@ -0,0 +1,44 @@
+import debug from 'debug';
+
+// Change this to your app name
+debug('webpack-express-boilerplate:server');
+
+export function normalizePort(val) {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/*
+ * Event listener for HTTP server "error" event
+ */
+
+export function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  let bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+    default:
+      throw error;
+  }
+}
